6bb440fc5b2d331b6726872b1b5488e777954b6a
Joon Jung <joonjung55@gmail.com>
2022-05-31 11:48:17 -0400

    Perfect Checkpoint

--- script.js
+++ script.js
@@ -1,49 +1,49 @@
-let drink = {
-  name: document.querySelector("section > div > h2"),
-  photo: document.querySelector("img"),
-  instructions: document.querySelector("p"),
-}
 const drinkList = document.getElementById('drink-list')
-document.getElementById('name-btn').addEventListener("click", findCocktail)
 document.getElementById('random').addEventListener("click", randomDrink)
-
-function drinkUpdate(data, info) {
-  drink.instructions.textContent = data.drinks[info].strInstructions;
-  drink.name.textContent = data.drinks[info].strDrink;
-  drink.photo.src =  data.drinks[info].strDrinkThumb;
-}
-
-function newDrink(data, info) {
-  return function() {
-      if (info < data.drinks.length - 1) info++
-      drinkUpdate(data, info)
-  }
-}
+document.getElementById('form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  findCocktail()  
+})
 
 function findCocktail() {
   fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + document.getElementById('name').value)
   .then(response => response.json())
   .then(data => {console.log(data) 
-    drinkUpdate(data, 0)
-      document.getElementById('newCocktail').addEventListener("click", newDrink(data, 0))
       renderDrinkNames(data.drinks)
   })
 }
  
 function randomDrink() {
+  clearDrink()
   fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
       .then(response => response.json())
-      .then(data => {console.log(data) 
-          drinkUpdate(data, 0)
+      .then(data => {renderDrinkNames(data.drinks)
       })
 }
 
 function renderDrinkNames(drinks) {
+  clearDrink()
   drinkList.textContent = ""
   drinks.forEach(drink => {
     console.log(drink)
     const drinkName = document.createElement('li')
     drinkName.textContent = drink.strDrink
+    drinkName.addEventListener('click', () => {
+      document.querySelector("p").textContent = drink.strInstructions;
+      document.querySelector("section > div > h2").textContent = drink.strDrink;
+      document.querySelector("img").src =  drink.strDrinkThumb;
+    })
     drinkList.append(drinkName)
   })
 }
+
+function clearDrink() {
+  document.querySelector("section > div > h2").textContent = "";
+  document.querySelector("img").src = "";
+  document.querySelector("p").textContent = "";
+}
+
+
+
+
+
