6458fbf9004a7e7e244467ec5a1cdcbd36655812
Joon Jung <joonjung55@gmail.com>
2022-06-01 11:04:52 -0400

    new save

--- script.js
+++ script.js
@@ -41,53 +41,22 @@ function renderDrinkNames(drinks) {
       document.querySelector("p").textContent = drink.strInstructions;
       document.querySelector("section > div > h2").textContent = drink.strDrink;
       document.querySelector("img").src =  drink.strDrinkThumb;
-      const step1 = document.createElement('li')
-            step1.textContent = `${drink.strIngredient1}, ${drink.strMeasure1}`
-
-            const step2 = document.createElement('li')
-            if(drink.strMeasure2 !== null) {
-                step2.textContent = `${drink.strIngredient2}, ${drink.strMeasure2}`
-            } else {step2.textContent = drink.strIngredient2}
-            
-
-            const step3 = document.createElement('li')
-            if(drink.strIngredient3 === null || drink.strIngredient3 === "") {
-                step3.style.display = "none"
-            } else if(drink.strMeasure3 !== null) {
-                step3.textContent = `${drink.strIngredient3}, ${drink.strMeasure3}`
-            } else {step3.textContent = drink.strIngredient3}
-            
-            
-            const step4 = document.createElement('li')
-            if(drink.strIngredient4 === null || drink.strIngredient4 === "") {
-                step4.style.display = "none"
-            } else if(drink.strMeasure4 !== null) {
-                step4.textContent = `${drink.strIngredient4}, ${drink.strMeasure4}`
-            } else {step4.textContent = drink.strIngredient4}
-
-
-            const step5 = document.createElement('li')
-            if(drink.strIngredient5 === null || drink.strIngredient5 === "") {
-                step5.style.display = "none"
-            } else if(drink.strMeasure5 !== null) {
-                step5.textContent = `${drink.strIngredient5}, ${drink.strMeasure5}`
-            } else {step5.textContent = drink.strIngredient5}
-
-            const step6 = document.createElement('li')
-            if(drink.strIngredient6 === null || drink.strIngredient6 === "") {
-                step6.style.display = "none"
-            } else if(drink.strMeasure6 !== null) {
-                step6.textContent = `${drink.strIngredient6}, ${drink.strMeasure6}`
-            } else {step6.textContent = drink.strIngredient6}
-            
-            const step7 = document.createElement('li')
-            if(drink.strIngredient7 === null || drink.strIngredient7 === "") {
-                step7.style.display = "none"
-            } else if(drink.strMeasure7 !== null) {
-                step7.textContent = `${drink.strIngredient7}, ${drink.strMeasure7}`
-            } else {step7.textContent = drink.strIngredient7}
-
-            ingredientList.append(step1, step2, step3, step4, step5, step6, step7)
+      
+      let n = 32
+            for(let i = 17 ; i < 31; i++) {
+                console.log(Object.entries(drink)[i][1])
+                //console.log(Object.entries(drink)[n][1])
+                const step = document.createElement('li')
+                if(Object.entries(drink)[i][1] === null || Object.entries(drink)[i][1] === "") {
+                    step.style.display = 'none'
+                } else if (Object.entries(drink)[n][1] === null) {
+                    step.textContent = Object.entries(drink)[i][1]
+                } else {
+                    step.textContent = `${Object.entries(drink)[i][1]}, ${Object.entries(drink)[n][1]}`
+                }
+                ingredientList.append(step)
+                n++
+            }
     })
     drinkList.append(drinkName)
   })
