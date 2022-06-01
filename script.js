b4cda41469dda483606ba9089276e6a3e3bc715d
Joon Jung <joonjung55@gmail.com>
2022-06-01 18:23:06 -0400

    new Save

--- script.js
+++ script.js
@@ -3,30 +3,24 @@ const ingredientList = document.getElementById('ingredient-list');
 document.getElementById('random').addEventListener("click", randomDrink)
 document.getElementById('form').addEventListener('submit', (e) => {
   e.preventDefault();
-  findCocktail()  
+  findCocktail()
+  document.getElementById('name').value = '';
 })
-
 function findCocktail() {
   fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + document.getElementById('name').value)
   .then(response => response.json())
-  .then(data => {console.log(data) 
-      renderDrinkNames(data.drinks)
-  })
+  .then(data => renderDrinkNames(data.drinks))
 }
- 
 function randomDrink() {
   clearDrink()
   fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
       .then(response => response.json())
-      .then(data => {renderDrinkNames(data.drinks)
-      })
+      .then(data => renderDrinkNames(data.drinks))
 }
-
 function renderDrinkNames(drinks) {
   clearDrink()
   drinkList.textContent = ""
   drinks.forEach(drink => {
-    console.log(drink)
     const drinkName = document.createElement('li')
     drinkName.textContent = drink.strDrink
     drinkName.addEventListener('mouseenter', () => {
@@ -41,18 +35,20 @@ function renderDrinkNames(drinks) {
       document.querySelector("p").textContent = drink.strInstructions;
       document.querySelector("section > div > h2").textContent = drink.strDrink;
       document.querySelector("img").src =  drink.strDrinkThumb;
-      
+      document.querySelector("#ingredient-container").style.visibility ="visible"
+      document.querySelector('#instructions-container').style.visibility = 'visible'
+      document.querySelector('#image').style.visibility = 'visible'
       let n = 32
             for(let i = 17 ; i < 31; i++) {
-                console.log(Object.entries(drink)[i][1])
-                //console.log(Object.entries(drink)[n][1])
                 const step = document.createElement('li')
-                if(Object.entries(drink)[i][1] === null || Object.entries(drink)[i][1] === "") {
+                const ingredient = Object.entries(drink)[i][1]
+                const measurement = Object.entries(drink)[n][1]
+                if(ingredient === null || ingredient === "") {
                     step.style.display = 'none'
-                } else if (Object.entries(drink)[n][1] === null) {
-                    step.textContent = Object.entries(drink)[i][1]
+                } else if (measurement === null) {
+                    step.textContent = ingredient
                 } else {
-                    step.textContent = `${Object.entries(drink)[i][1]}, ${Object.entries(drink)[n][1]}`
+                    step.textContent = `${ingredient}, ${measurement}`
                 }
                 ingredientList.append(step)
                 n++
@@ -61,15 +57,9 @@ function renderDrinkNames(drinks) {
     drinkList.append(drinkName)
   })
 }
-
 function clearDrink() {
   document.querySelector("section > div > h2").textContent = "";
   document.querySelector("img").src = "";
   document.querySelector("p").textContent = "";
   ingredientList.textContent = ""
 }
-
-
-
-
-
